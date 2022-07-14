3d0465f8f0e0fc8a92e1396987f3742ea5be3364
Andre Burke <aburke.dev@gmail.com>
2022-07-14 01:56:22 -0600

    making updates to all files

--- lib/cdk-timer-app-stack.ts
+++ lib/cdk-timer-app-stack.ts
@@ -1,16 +1,12 @@
-import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import {
-  AttributeType,
-  BillingMode,
-  Table,
-} from "aws-cdk-lib/aws-dynamodb";
-import { Secret } from "aws-cdk-lib/aws-secretsmanager";
-import { Construct } from "constructs";
-import { GitHubRepoInsert } from "./github-repo-insert";
-import { GitHubRepoRead } from "./github-repo-read";
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { Construct } from 'constructs';
+import { GitHubRepoInsert } from './github-repo-insert';
+import { GitHubRepoRead } from './github-repo-read';
 
 const memoryAndTimeout = {
-  memorySize: 256,
+  memorySize: 512,
   timeout: Duration.minutes(3),
 } as const;
 
@@ -18,33 +14,24 @@ export class CdkTimerAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const gitHubRepoTable = new Table(this, "GitHubRepoTable", {
-      partitionKey: { name: "id", type: AttributeType.STRING },
-      sortKey: { name: "createdAt", type: AttributeType.STRING },
+    const gitHubRepoTable = new Table(this, 'GitHubRepoTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      sortKey: { name: 'createdAt', type: AttributeType.STRING },
       billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const gitHubUserSecret = Secret.fromSecretNameV2(
-      this,
-      "GitHubUserSecret",
-      "GitHubUser"
-    );
+    const gitHubUserSecret = Secret.fromSecretNameV2(this, 'GitHubUserSecret', 'GitHubUser');
+    const gitHubPatSecret = Secret.fromSecretNameV2(this, 'GitHubPatSecret', 'GitHubPat');
 
-    const gitHubPatSecret = Secret.fromSecretNameV2(
-      this,
-      "GitHubPatSecret",
-      "GitHubPat"
-    );
-
-    new GitHubRepoInsert(this, "GitHubRepoInsert", {
+    new GitHubRepoInsert(this, 'GitHubRepoInsert', {
       memoryAndTimeout,
       gitHubRepoTable,
       gitHubUserSecret,
       gitHubPatSecret,
     });
 
-    new GitHubRepoRead(this, "GitHubRepoRead", {
+    new GitHubRepoRead(this, 'GitHubRepoRead', {
       memoryAndTimeout,
       gitHubRepoTable,
       gitHubUserSecret,
--- lib/github-repo-read.ts
+++ lib/github-repo-read.ts
@@ -1,12 +1,12 @@
-import { Size } from "aws-cdk-lib";
-import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
-import { Table } from "aws-cdk-lib/aws-dynamodb";
-import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { ISecret } from "aws-cdk-lib/aws-secretsmanager";
-import { MemoryDB } from "aws-sdk";
-import { Construct } from "constructs";
-import { MemoryAndTimout } from "./utils/types";
+import { Size } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { ISecret } from 'aws-cdk-lib/aws-secretsmanager';
+import { MemoryDB } from 'aws-sdk';
+import { Construct } from 'constructs';
+import { MemoryAndTimout } from './utils/types';
 
 export interface GitHubRepoReadProps {
   memoryAndTimeout: MemoryAndTimout;
@@ -19,31 +19,26 @@ export class GitHubRepoRead extends Construct {
   constructor(scope: Construct, id: string, props: GitHubRepoReadProps) {
     super(scope, id);
 
-    const handler = new NodejsFunction(this, "Handler", {
-      memorySize: 128,
+    const handler = new NodejsFunction(this, 'Handler', {
+      memorySize: 256,
       timeout: props.memoryAndTimeout.timeout,
       runtime: Runtime.NODEJS_14_X,
-      handler: "handler",
-      entry:
-        Code.fromAsset("lambda").path + "/github-repo-read-function.ts",
+      handler: 'handler',
+      entry: Code.fromAsset('lambda').path + '/github-repo-read-function.ts',
       bundling: {
         minify: true,
-        externalModules: ["aws-sdk"],
+        externalModules: ['aws-sdk'],
       },
       environment: {
-        GITHUB_USER: props.gitHubUserSecret.secretValue
-          .unsafeUnwrap()
-          .toString(),
-        GITHUB_PAT: props.gitHubPatSecret.secretValue
-          .unsafeUnwrap()
-          .toString(),
+        GITHUB_USER: props.gitHubUserSecret.secretValue.unsafeUnwrap().toString(),
+        GITHUB_PAT: props.gitHubPatSecret.secretValue.unsafeUnwrap().toString(),
         TABLE_NAME: props.gitHubRepoTable.tableName,
       },
     });
 
     props.gitHubRepoTable.grantReadData(handler);
 
-    new LambdaRestApi(this, "Endpoint", {
+    new LambdaRestApi(this, 'Endpoint', {
       handler: handler,
     });
   }
