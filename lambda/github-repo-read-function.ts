2371ffdac134b9230019a0d6270c0e84c5260a8b
Andre Burke <aburke.dev@gmail.com>
2022-07-26 18:15:55 -0600

    refined the api to one resource

--- lambda/github-repo-read-function.ts
+++ lambda/github-repo-read-function.ts
@@ -2,33 +2,26 @@ import AWS = require('aws-sdk');
 import { DynamoDB } from 'aws-sdk';
 import { APIGatewayEvent } from 'aws-lambda';
 import { getProjectsFromDynamoDB, parseGitHubProjectsFromDynamoDB } from '../lib/services/dynamo-db-service';
-import { isAllowedOrigin } from '../lib/services/http-service';
 
 AWS.config.update({ region: 'us-west-2' });
 
 const apiVersion = { apiVersion: '2012-08-10' };
-const defaultOrigin = 'https://www.aburke.tech';
+
+const headers = {
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET',
+} as const;
+
+const response = {
+  statusCode: 200,
+  headers: headers,
+  body: {},
+};
 
 let ddb = new DynamoDB(apiVersion);
 
 exports.handler = async (event: APIGatewayEvent) => {
-  const origin = event.headers.origin ?? 'N/A';
-  let goodOrigin = isAllowedOrigin(origin);
-
-  console.info('Origin', origin);
-
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Headers':
-        'Accept,Accept-Language,Content-Language,Content-Type,Authorization,x-correlation-id',
-      'Access-Control-Expose-Headers': 'x-my-header-out',
-      'Access-Control-Allow-Methods': 'GET',
-      'Access-Control-Allow-Origin': goodOrigin ? origin : defaultOrigin,
-    } as const,
-    body: {},
-  };
-
   try {
     if (!ddb) {
       ddb = new DynamoDB(apiVersion);
